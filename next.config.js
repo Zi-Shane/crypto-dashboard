1370048d21138b9d92876c8e6b96e43110498768
Shane Huang <shane@Shanes-Laptop.local>
2024-01-14 20:47:25 +0800

    fix:

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   typescript: {
     tsconfigPath: './tsconfig.json',
   },
+  baseUrl: 'app',
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
