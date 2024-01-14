e55bc97585a1d2461719a4dc3f6f9e1be5e3c758
Shane Huang <shane@Shanes-Laptop.local>
2024-01-14 23:11:55 +0800

    fix:

--- next.config.js
+++ next.config.js
@@ -1,24 +1,32 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+// const path = require('path');
 const nextConfig = {
   output: 'export',
   typescript: {
     tsconfigPath: './tsconfig.json',
   },
-  // basePath: 'app',
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'app'),
-    };
-    if (!isServer) {
-      config.resolve.fallback = {
-        utilies: 'app/utilies',
-      };
-    }
-
-    return config;
+  // Use the prefix for GitHub Pages
+  basePath: '/crypto-dashboard',
+  assetPrefix: '/crypto-dashboard/',
+  // Enable static export
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
+  // basePath: 'app',
+  // webpack: (config, { isServer }) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@': path.resolve(__dirname, 'app'),
+  //   };
+  //   if (!isServer) {
+  //     config.resolve.fallback = {
+  //       utilies: 'app/utilies',
+  //     };
+  //   }
+
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
